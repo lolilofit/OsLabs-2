b05ae1de1e5d2c1f55d51283bfdac8229e3972ab
Usova Daria Sergeevna <usova@ccfit.nsu.ru>
2019-11-02 11:24:27 +0700

    proxy http1.0

--- 29/lab29_bl.c
+++ 29/lab29_bl.c
@@ -32,7 +32,7 @@ struct HttpAnswer {
 };
 
 struct List {
-	char str[MAX_HEADER_SIZE+1];
+	char str[MAX_HEADER_SIZE+MAX_BODY_SIZE+1];
 	struct List* next;
 };
 
@@ -42,7 +42,64 @@ struct ClientHostList {
 	struct ClientHostList* next;
 };
 
-void divide_body(char* response, struct HttpAnswer* answer) {
+
+struct CacheUnit {
+	struct List* mes_head;
+	char* url;
+	int waiting_now;
+	int is_downloaded;
+	int id;
+	struct CacheUnits* next;
+	//int popularity;
+};
+
+
+struct Cache {
+	struct CacheUnit* units_head;
+	int max_id;
+};
+
+void init_cache_unit(struct CacheUnit* cache_unit, int id, char* url, int url_size) {
+ 	cache_unit = (struct CacheUnit*)malloc(sizeof(struct CacheUnit));
+        cache_unit->id = id;
+	cache_unit->NULL;
+        cache_unit->waiting_now = 1;
+        cache_unit->is_downloading = 1;
+	cache_unit->mes_head = (struct List*)malloc(sizeof(struct List));
+	cache_unit->mes_head->next=NULL;
+	if(url_size != 0) {
+		 cache_unit->url = (char*)malloc(sizeof(char)*url_size);
+	}
+	else {
+		 cache_unit->url=NULL;
+	}
+}
+
+void init_cache(struct Cache* cache) {
+	cache = (struct Cache*)malloc(sizeof(struct Cache));
+	cache->max_id = 0;
+	init_cache_head(cache->units_head, 0);
+}
+
+void dealloc_cache(struct Cache* cache) {
+	struct CacheUnit* unit = cache->units_head;
+	struct CacheUnit* prev;
+	while(unit) {
+		struct List* cur_list = unit->mes_head;
+		while(cur_list != NULL) {
+			free(cur_list);
+			cur_list=cur_list->next;
+		}
+		if(unit->url != NULL)
+			free(unit->url);
+		prev = unit;
+		unit = unit->next;
+		free(prev);
+	}
+}
+
+
+int divide_body(char* response, struct HttpAnswer* answer) {
 	char *tmp;
 	char part[MAX_HEADER_SIZE+1];
 
@@ -51,8 +108,10 @@ void divide_body(char* response, struct HttpAnswer* answer) {
 		strcpy(answer->body, tmp + strlen("\r\n\r\n"));
 		strncpy(answer->header, response, (tmp-response));
 		//strcat(answer->header, part);
-		response[tmp-response]='\0';
+		//if(tmp < strlen(response))
+		//	return 1;
 	}
+	return 0;
 //	printf("head: %s\nbody: %s\n", answer->header, answer->body);
 }
 
@@ -210,11 +269,13 @@ int get_remote_socket(char* host, char* port) {
 
 
 void form_http_request(struct HttpParams* response, char* header) {
-	sprintf(header, "%s %s %s\r\nHost: %s\r\n\r\n", response->method, response->path, response->protocol, response->host);
+	// response->protocol;
+	sprintf(header, "%s %s %s\r\nHost: %s\r\n\r\n", response->method, response->path, "HTTP/1.0", response->host);
 //	printf("header: %s\n", header);
 }
 
 struct ClientHostList* find_related(struct ClientHostList* head, int find_him) {
+//	printf("find related\n");
 	struct ClientHostList* cur = head->next;
 	struct ClientHostList* prev = head;
 	while(cur != NULL) {
@@ -282,24 +343,25 @@ int hexadecimalToDecimal(char hexVal[]) {
     return dec_val;
 }
 
-
+/*
 int transfer_back(struct ClientHostList* related) {
 	printf("transfer back\n");
 
 	char buf[MAX_HEADER_SIZE + MAX_BODY_SIZE+1] = "";
 	int remote_host = related->remote_host;
-	int client = related->client, flag = 0,  mes_len;
+	int client = related->client, flag = 0,  mes_len, is_enough = 0;
 	int try_read = 0, readen;
 	struct HttpAnswer* ans;
         ans = (struct HttpAnswer*)malloc(sizeof(struct HttpAnswer));
 	(ans->body)[0] = '\0';
 
-	while(strlen(ans->body) == 0) {
-	printf("continue reading head\n\n");
+	//find empty str
+	while(strlen(ans->body) == 0 && is_enough == 0) {
+	printf("continue reading head\n\n======================\n\n");
         readen = read(remote_host, buf, MAX_HEADER_SIZE+MAX_BODY_SIZE);
         buf[readen+1] = '\0';
 
-	printf("got:\n%s\n\n", buf);
+	printf("got in head part:\n%s\n\n", buf);
 	if(readen < 0) {
                 printf("error reading from remote host");
                 return -1;
@@ -312,7 +374,9 @@ int transfer_back(struct ClientHostList* related) {
 
         ans->chunked = 0;
 	ans->content_lengh=-1;
-	divide_body(buf, ans);
+	if(divide_body(buf, ans) == 1)
+		is_enough
+	printf("---------------------\nbody is: %s\n==============\n\n", ans->body);
 
         try_read+=strlen(ans->body);
         parse_request(ans->header, NULL, ans);
@@ -332,9 +396,10 @@ int transfer_back(struct ClientHostList* related) {
 			mes_len = dec_number;
 		}
 	}
-	printf("TIME to READ the rest\n\n");
+//	printf("TIME to READ the rest\n\n");
 	//if we haven't readen all head
 	}
+	printf("TIME to READ the rest\n\n");
         while(flag == 1 || try_read < mes_len) {
 		//char buf1[MAX_HEADER_SIZE + MAX_BODY_SIZE+1] = "";
 		readen = read(remote_host, buf, MAX_HEADER_SIZE+MAX_BODY_SIZE);
@@ -350,7 +415,7 @@ int transfer_back(struct ClientHostList* related) {
                 	printf("can't write to remote host\n");
         	        return -1;
 	        }
-                printf("again readen:\n\n%s", buf);
+                printf("\n\nagain readen:\n\n%s", buf);
         }
 
 	if(ans->chunked == 1 && mes_len != 0) {
@@ -360,15 +425,109 @@ int transfer_back(struct ClientHostList* related) {
 	free(ans);
 	return 0;
 }
+*/
+
+
+int transfer_back(struct ClientHostList* related) {
+	char buf[MAX_HEADER_SIZE + MAX_BODY_SIZE+1] = "";
+	int remote_host = related->remote_host;
+	int client = related->client;
+	int try_read = 0, readen;
+        //copy buf??
+        readen = read(remote_host, buf, MAX_HEADER_SIZE+MAX_BODY_SIZE);
+	 buf[readen+1] = '\0';
+
+	if(readen < 0) {
+                printf("error reading from remote host");
+                return -1;
+        }
+
+	if(readen == 0) { 
+		printf("\n now it's null\n");
+		return 1;
+	}
+	buf[7] = '0';
+	printf("got:\n%s\n\n", buf);
+
+        if(write(client, buf, strlen(buf)) < 0) {
+                printf("can't write to remote host\n");
+                return -1;
+        }
+        struct HttpAnswer* ans;
+        ans = (struct HttpAnswer*)malloc(sizeof(struct HttpAnswer));
+        (ans->body)[0] = '\0';
+	ans->chunked = 0;
+	ans->content_lengh = -1;
+	divide_body(buf, ans);
+
+        try_read+=strlen(ans->body);
+//      printf("readen bytes - %d and body - %d\n", readen, strlen(ans->body));
+        parse_request(ans->header, NULL, ans);
+//      printf("content-len: %d answer %s\n", ans->content_lengh, ans->status);
+
+	printf("divided, chunced=%d\n", ans->chunked);
+	printf("readen now : %d of %d\n\n", try_read, ans->content_lengh);
+
+	int mes_len =  ans->content_lengh, flag = 0, dec_number = 0;
+/*	if((ans->chunked) == 1) {
+		char number[1024] = "";
+		sscanf(ans->body, "%s\n", number);
+		dec_number = hexadecimalToDecimal(number);
+		printf("CHUNKED, first time : %s\n", number);
+		if(strcmp(number, "") == 0) {
+			flag = 1;
+		}
+		else {
+			mes_len = dec_number;
+		}
+	}
+*/
+
+        while(try_read < mes_len) {
+		//char buf1[MAX_HEADER_SIZE + MAX_BODY_SIZE+1] = "";
+		readen = read(remote_host, buf, MAX_HEADER_SIZE+MAX_BODY_SIZE);
+                buf[readen+1]='\0';
+		if(readen < 0) {
+                        printf("error reading from remote host-additional read\n");
+                        return -1;
+                }
+                if(readen == 0)
+                        break;
+                try_read += readen;
+		/*
+		if(flag == 1) {
+			char number[1024] = "";
+			sscanf(buf, "%s\n", number);
+			dec_number = hexadecimalToDecimal(number);
+        	        printf("CHUNKED : hex-%s, dec-%d\n", number, dec_number);
+			flag = 0;
+			mes_len = dec_number;
+		}
+		*/
+		if(write(client, buf, strlen(buf)) < 0) {
+                	printf("can't write to remote host\n");
+        	        return -1;
+	        }
+                printf("again readen:\n\n%s", buf);
+        }
+
+	if(mes_len < 0) {
+		free(ans);
+		return 2;
+	}
+	free(ans);
+	return 0;
+}
+
 
 struct ClientHostList* remove_conn_info(struct pollfd* fds, int i, struct ClientHostList* prev, struct ClientHostList* related,  struct ClientHostList* last) {
 	//if(fds[i].fd >= 0)
         //      close(fds[i].fd);
         //fds[i].fd = -1;
-	printf("remove conn info\n");
+	printf("remove_conn_info\n");
 	printf("prev cli %d, host %d\n", prev->client, prev->remote_host);
         prev->next = related->next;
-        free(related);
+//        free(related);
 	if(prev->next == NULL)
 		return prev;
 	return last;
@@ -446,7 +605,7 @@ int main(int argc, char* argv[]) {
 	last = head;
 
 	while(1) {
-		printf("try poll\n");
+	//	printf("try poll\n");
 		ret = poll(fds, nfd, timeout);
 		printf("poll!!\n");
 		if(ret < 0) {
@@ -458,26 +617,23 @@ int main(int argc, char* argv[]) {
 			exit(1);
 		}
 		size_copy = nfd;
-		printf("size_copy %d\n", size_copy);
+	//	printf("size_copy %d\n", size_copy);
 		for(i = 0; i < size_copy; i++) {
 			//char buf[MAX_HEADER_SIZE + MAX_BODY_SIZE];
-			printf("again\n");
-
-
 			if(fds[i].revents == 0) {
-				printf("events = 0\n");
+			//	printf("events = 0\n");
 				continue;
 			}
 			//delete this fd
 			if(fds[i].revents != POLLIN) {
-				 printf("events not pollin\n");
+			//	 printf("events not pollin\n");
 				continue;
 			}
-			printf("Oh!\n");
+			//printf("Oh\n");
 			if(fds[i].fd == sc) {
 				//it's me
 				client = 0;
-				while(client != -1) { 
+				//while(client != -1) { 
 					//printf("try accept self=%d, socket-to-acc=%d\n", sc, fds[i].fd);
 					if((client = accept(fds[i].fd, (struct sockaddr*)&sc_addr, (socklen_t*)&addrlen)) < 0) {
                 				//printf("error accept\n");
@@ -501,23 +657,39 @@ int main(int argc, char* argv[]) {
 					new_client->next=NULL;
 					last->next = new_client;
 					last = new_client;
-
-					printf("accepting sucess %d\n", client);
-				}
+					
+					for(j = 0; j < nfd; j++) {
+						printf("fd: %d\n", fds[j].fd);
+					}
+					printf("accepting sucess %d, nfd=%d\n", client, nfd);
+				//}
 			}
 			else {
-					//printf("message nfd before mes - %d!\n", nfd);
+					if(head == NULL)
+						printf("head is null\n");
 					struct ClientHostList* prev = find_related(head, fds[i].fd);
+					if(prev == NULL)
+						printf("prev is null\n");
 					struct ClientHostList* related = prev->next;
 
 					if(related->client == fds[i].fd) {
-						printf("message came for %d (to host)\n\n", fds[i].fd);
+						 printf("message came for %d (to host), and host is %d, client %d\n\n", fds[i].fd, related->remote_host, related->client);
 						if(related->remote_host > 0) {
-							char buf[4];
-							if (recv(related->remote_host, buf, sizeof(buf), MSG_PEEK | MSG_DONTWAIT) == 0) {
-								printf("should close\n");
+                                                        char buf[4];
+                                                        if ((recv(related->remote_host, buf, sizeof(buf), MSG_PEEK | MSG_DONTWAIT)) == -1) {
+                                                                printf("should close\n");
+								//close(fds[i].fd);
+								fds[i].fd = -1;
+								/*for(j = 0; j < nfd; j++) {
+									if(fds[j].fd == related->remote_host) {
+										close(fds[j].fd);
+										fds[j].fd=-1;
+									}
+								}
+								*/
+                                                        	last = remove_conn_info(fds, i, prev, related, last);
 							}
-						}
+                                                }
 						else {
 						if(transfer_to_remote(related, fds, nfd) == -1) {
 							//remove from fds and list
@@ -528,13 +700,19 @@ int main(int argc, char* argv[]) {
 						}
 					}
 					else {
+						  printf("message came for %d (back to cli)\n\n", fds[i].fd);
 						if(related->remote_host == fds[i].fd) {
-							int answ = transfer_back(related);
-							if(answ != 2) {
+							if(transfer_back(related) != 2) {
 
-							printf("transfer back succ\n");
+							printf("transfer back succ, socket %d\n", fds[i].fd);
 							close(fds[i].fd);
-							//fds[i].fd=-1;
+							fds[i].fd=-1;
+							for(j = 0; j < nfd; j++) {
+                                                                        if(fds[j].fd == related->client) {
+                                                                                close(fds[j].fd);
+                                                                                fds[j].fd=-1;
+                                                                        }
+                                                                }
 
 							//struct ClientHostList* cur2 = head;
 							//while(cur2 != NULL) {
@@ -542,8 +720,7 @@ int main(int argc, char* argv[]) {
 							//	cur2 = cur2->next;
 							//}
  							// printf("last cli %d, host %d\n\n", last->client, last->remote_host);
-							
-							//last = remove_conn_info(fds, i, prev, related, last);
+							last = remove_conn_info(fds, i, prev, related, last);
 							printf("DELETED\n");
 							}
 						}
@@ -552,7 +729,12 @@ int main(int argc, char* argv[]) {
 					}
 					}
 			}
+			
 		}
+		nfd = evacuate_fds(fds);
+		for(i = 0; i < nfd; i++)
+			printf("fds: %d, ", fds[i]);
+		printf("\n\n");
 	}
 	for(i = 0; i < nfd; i++) {
 		if(fds[i].fd >= 0) 
